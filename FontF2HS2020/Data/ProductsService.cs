22b5337620b173d86c1e628365cbfd67b5b794f7
Fabian <fabianp60@gmail.com>
2020-02-08 13:20:23 -0500

    Agregando proyecto Web Blazor

---
+++ FontF2HS2020/Data/ProductsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FontF2HS2020.Data
+{
+    public class ProductsService
+    {
+        private static readonly string[] ProductNames = new[]
+        {
+            "Zapatos", "Ropa", "Celulares", "Computadores", "Medias", "Maletines"
+        };
+
+        public Task<string[]> GetProductNamesAsync(string searchText)
+        {
+            return Task.Run(() => { return ProductNames; });
+        }
+    }
+}
