cf83580161216e93df8042eabbeb00ca5a023236
Fabian <fabianp60@gmail.com>
2020-02-09 09:28:28 -0500

    Validacion del archivo en cliente previo al cargue masivo

--- FrontHS2020MVC/wwwroot/js/Products/MasiveUpload.js
+++ FrontHS2020MVC/wwwroot/js/Products/MasiveUpload.js
@@ -1,14 +1,15 @@
 ﻿function MasiveUploadProductsFrontControl() {
-	this.InitControl();
-	this.InitEventListeners();
+    this.InitControl();
+    this.InitEventListeners();
 }
 
 MasiveUploadProductsFrontControl.prototype.InitControl = function () {
-	this._inputFile = document.getElementById('products-file');
+    this._inputFile = document.getElementById('products-file');
+    this._fileResults = document.getElementById('file-content');
 };
 
 MasiveUploadProductsFrontControl.prototype.InitEventListeners = function () {
-	this._inputFile.addEventListener('change', this.OnInputFileChangeEvent.bind(this), false);
+    this._inputFile.addEventListener('change', this.OnInputFileChangeEvent.bind(this), false);
 };
 
 MasiveUploadProductsFrontControl.prototype.OnInputFileChangeEvent = function (evt) {
@@ -16,16 +17,88 @@ MasiveUploadProductsFrontControl.prototype.OnInputFileChangeEvent = function (ev
     if (!file) {
         return;
     }
+    this._fileResults.innerHTML = "Cargando el archivo...";
+    this._currentFile = file;
     const fileReader = new FileReader();
-    fileReader.onload = function (e) {
-        let fileContent = e.target.result;
-        var fileLines = fileContent.split(/(?:\r\n|\r|\n)/g);
-        document.getElementById('file-content').innerHTML = fileContent.replace(/(?:\r\n|\r|\n)/g, '<br>');
-        console.log(fileLines);
-    };
+    fileReader.onload = this.OnFileLoaded.bind(this);
+    fileReader.onerror = this.OnFileError.bind(this);
     fileReader.readAsText(file);
 };
 
+MasiveUploadProductsFrontControl.prototype.OnFileLoaded = function (evt) {
+    let fileContent = evt.target.result;
+    let fileLines = fileContent.split(/(?:\r\n|\r|\n)/g);
+    // FIleIsValid Muestra las lineas con error
+    if (this.FileIsValid(fileLines)) {
+        this.RenderTable(fileLines);
+    }
+};
 
+MasiveUploadProductsFrontControl.prototype.FileIsValid = function (fileLines) {
+    // TODO: validar cada linea e indicar si alguna linea no cumple las caracteristicas
+    let isValid = true;
+    this._fileResults.innerHTML = "Validando el archivo...<br>";
+    let errors = '';
+    for (let i = 0; i < fileLines.length; i++) {
+        let columns = fileLines[i].split('\t');
+        if (columns.length != 4) {
+            errors += `La linea ${i + 1} tiene ${columns.length} columnas deben ser 4<br>`;
+            isValid = false;
+        }
+        if (isNaN(parseInt(columns[0]))) {
+            errors += `En la linea ${i + 1} la columna Tenant_id no contiene un entero<br>`;
+            isValid = false;
+        }
+        if (columns[1].length == 0) {
+            errors += `En la linea ${i + 1} la columna Name está vacía y es obligatoria<br>`;
+            isValid = false;
+        }
+        if (isNaN(parseFloat(columns[3]))) {
+            errors += `En la linea ${i + 1} la columna List_price no contiene un número decimal<br>`;
+            isValid = false;
+        }
+    }
+    if (!isValid) {
+        this._fileResults.innerHTML += `<div class="alert alert-danger" role="alert">${errors}</div>`;
+    } else {
+        this._fileResults.innerHTML += `<div class="alert alert-success" role="alert">¡Es valido!</div>`;
+    }
+    return isValid;
+};
+
+MasiveUploadProductsFrontControl.prototype.RenderTable = function (fileLines) {
+    // tenantid,name,description,list_price
+    let tableHtml = `<table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                              <th scope="col">Tenant_id</th>
+                              <th scope="col">Name</th>
+                              <th scope="col">Description</th>
+                              <th scope="col">List_price</th>
+                              <th scope="col">¿Cargado?</th>
+                            </tr>
+                          </thead>
+                        <tbody>`;
+    for (let i = 0; i < fileLines.length; i++) {
+        let columns = fileLines[i].split('\t');
+        tableHtml += `<tr>
+                        <td>${columns[0]}</td>
+                        <td>${columns[1]}</td>
+                        <td>${columns[2]}</td>
+                        <td>${columns[3]}</td>
+                        <td>Esperando...</td>
+                    </tr>`;
+    }
+
+    tableHtml += `</tbody>
+                </table>`;
+
+    this._fileResults.innerHTML = tableHtml;
+};
+
+MasiveUploadProductsFrontControl.prototype.OnFileError = function (evt) {
+    this._fileResults.innerHTML = `<div class="alert alert-danger" role="alert">Se presentó un error, no se pudo cargar el archivo</div>`;
+    console.log(evt);
+};
 
 const masiveUpload = new MasiveUploadProductsFrontControl();
